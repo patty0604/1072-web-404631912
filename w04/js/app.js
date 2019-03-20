f0e07064e109809c4a2ce877299fe8949b3e0ed0
patty0604 <patty860604@gmail.com>
2019-03-20 13:25:06 +0800

    w04 files

---
+++ w04/js/app.js
@@ -0,0 +1,10 @@
+alert('HI');/*彈出視窗 */
+let input =prompt('enter your name');/*input為變數值 做輸入動作*/ 
+console.log(input);
+
+/*在console打document.head就會把程式碼head的部分抓出來 */
+/* document.link[0].innerText='HELLO';改超連結的程式*/
+/*document.querySelector('#special')找class special 的程式碼*/
+/*document.querySelector('#special').innerHTML改class special裡的程式 */
+/*document.querySelectorAll('ul li')找全部的ul和li*/
+/*window.location */
